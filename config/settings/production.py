08529278e06b319828e2fe4372e1a1d29d08c479
mvaughan <magnus.vaughan@gmail.com>
2022-05-01 09:29:36 +0100

    Added allowed cors

--- config/settings/production.py
+++ config/settings/production.py
@@ -185,3 +185,6 @@ sentry_sdk.init(
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+CORS_ALLOWED_ORIGINS = [
+    "https://626e434ea152cf1d75375d48--majestic-mermaid-b67170.netlify.app"
+]
