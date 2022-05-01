3b8dd40e9a3e3403adac7f64d059d6340de83b1d
mvaughan <magnus.vaughan@gmail.com>
2022-05-01 17:30:13 +0100

    Removed slash from CORS allowed

--- config/settings/production.py
+++ config/settings/production.py
@@ -185,7 +185,7 @@ sentry_sdk.init(
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-CORS_ALLOWED_ORIGINS = ["https://majestic-mermaid-b67170.netlify.app/"]
+CORS_ALLOWED_ORIGINS = ["https://majestic-mermaid-b67170.netlify.app"]
 
 # CORS_ORIGIN_WHITELIST = (
 #     "https://majestic-mermaid-b67170.netlify.app/",
